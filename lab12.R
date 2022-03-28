d40982659d6d301d117c7a0d7d6c89bc4b7a6de1
Zany <star3175@gmail.com>
2022-03-28 10:07:44 +0900

    clear

--- lab12.R
+++ lab12.R
@@ -1,31 +1,3 @@
-library(RSelenium)
-
-url <- "https://www.megabox.co.kr/movie-detail/comment?rpstMovieNo=21049700"
-remDr$navigate(url)
-
-df <- NULL
-
-for (i in 1:10+1) {
-  moviegradenodes <- remDr$findElements(using = "css selector", paste0("#contentData > div > div.movie-idv-story > ul > li:nth-child(", i, ") > div.story-area > div.story-box > div > div.story-cont > div.story-point > span"))
-  moviegrade <- sapply(moviegradenodes, function(x) {x$getElementText()})
-  
-  moviegoodsnodes <- remDr$findElements(using = "css selector", paste0("#contentData > div > div.movie-idv-story > ul > li:nth-child(", i, ") > div.story-area > div.story-box > div > div.story-cont > div.story-recommend > em"))
-  moviegoods <- list(sapply(moviegoodsnodes, function(x) {x$getElementText()}))
-  
-  moviecommentnodes <- remDr$findElements(using = "css selector", paste0("#contentData > div > div.movie-idv-story > ul > li:nth-child(", i, ") > div.story-area > div.story-box > div > div.story-cont > div.story-txt"))
-  moviecomment <- sapply(moviecommentnodes, function(x) {x$getElementText()})
-
-  df <- data.frame(grade=unlist(moviegrade), goods=unlist(moviegoods), comment=unlist(moviecomment))
-}
-
-df
-
-
-
-
-
-#
-
 movie_gradev = NULL
 movie_goodsv = NULL
 movie_commentv = NULL
@@ -74,7 +46,7 @@ for (i in 2:10) {
 }
 movie_df <- data.frame(movie_gradev, movie_goodsv, movie_commentv)
 View(movie_df)
-write.csv(movie_df, file = "oultput/movie.csv")
+write.csv(movie_df, file = "output/movie.csv")
   
   
   
